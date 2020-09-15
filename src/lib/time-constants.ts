c80ca66fe48ad832985528a6275c84f53dec96eb
Aaron Adrian <aaroncadrian@gmail.com>
2020-09-14 19:34:30 -0500

    Add constants (#1)
    * Install typescript
    
    * Configure tsconfig.json
    
    * Create time-constants.ts
    
    * Add build script
    
    * Add .npmignore

---
+++ src/lib/time-constants.ts
@@ -0,0 +1,70 @@
+/**
+ * PLURAL CONSTANTS
+ */
+
+/**
+ * MILLISECONDS is the number of milliseconds in a millisecond.
+ */
+export const MILLISECONDS = 1;
+
+/**
+ * SECONDS is the number of milliseconds in a second.
+ */
+export const SECONDS = 1000 * MILLISECONDS;
+
+/**
+ * MINUTES is the number of milliseconds in a minute.
+ */
+export const MINUTES = 60 * SECONDS;
+
+/**
+ * HOURS is the number of milliseconds in a hour.
+ */
+export const HOURS = 60 * MINUTES;
+
+/**
+ * DAYS is the number of milliseconds in a day.
+ */
+export const DAYS = 24 * HOURS;
+
+/**
+ * WEEKS is the number of milliseconds in a week.
+ */
+export const WEEKS = 7 * DAYS;
+
+
+/**
+ * SINGULAR ALIASES
+ */
+
+/**
+ * MILLISECOND is the number of milliseconds in a millisecond.
+ */
+export const MILLISECOND = MILLISECONDS;
+
+
+/**
+ * SECOND is the number of milliseconds in a second.
+ */
+export const SECOND = SECONDS;
+
+/**
+ * MINUTE is the number of milliseconds in a minute.
+ */
+export const MINUTE = MINUTES;
+
+/**
+ * HOUR is the number of milliseconds in a hour.
+ */
+export const HOUR = HOURS;
+
+/**
+ * DAY is the number of milliseconds in a day.
+ */
+export const DAY = DAYS;
+
+/**
+ * WEEK is the number of milliseconds in a week.
+ */
+export const WEEK = WEEKS;
+
